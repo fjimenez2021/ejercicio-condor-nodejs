9f9f9e8bc06a3d3e9b258d7419b9c7a836fc588a
Ferney Jimenez <fjimenez@condorlabs.io>
2021-01-27 18:44:56 -0500

    Se agrega otro test para validar si el problema es por la conexión a la BD

---
+++ __tests__/suma.spec.js
@@ -0,0 +1,16 @@
+
+beforeAll(async (done) => {
+    done();
+});
+
+afterAll(async (done) => {
+    done();
+});
+
+describe("Test suma", () => {
+    test("it should sum two num", async (done) => {
+        let input = (2 + 2);
+        expect(input).toEqual(4);
+        done();
+    });
+});

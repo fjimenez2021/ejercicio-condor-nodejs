c3448ecd55b1364da335971a2819eb40f2868d36
Ferney Jimenez <fjimenez@condorlabs.io>
2021-02-01 21:11:27 -0500

    Se ajusta config.js para que no muestre error si no encuentra el archivo .env

--- config/config.js
+++ config/config.js
@@ -1,8 +1,19 @@
 import dotenv from "dotenv";
 
-const result = dotenv.config();
-if (result.error) {
-    throw result.error;
+try {
+    const result = dotenv.config({silent: true});
+    if (result.error) {
+        throw result.error;
+    }
+} catch (error) {
+    console.log("Not found .env >>")
 }
-const { parsed: envs } = result;
-module.exports = envs;
+/*const { env: envs } = process;
+module.exports = envs;*/
+module.exports = {
+    MYSQL_HOST: process.env.MYSQL_HOST || '127.0.0.0', 
+    MYSQL_USER: process.env.MYSQL_USER || 'root', 
+    MYSQL_PASSWORD: process.env.MYSQL_PASSWORD || '',
+    MYSQL_DB_NAME: process.env.MYSQL_DB_NAME || 'mysql',
+    MYSQL_PORT: process.env.MYSQL_PORT || 3306
+};
